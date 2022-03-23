e9e21a695333788b666c350e12d8e335b5281f7f
Hao Xu <b.hao.xu@gmail.com>
2022-03-23 12:24:05 +1100

    Added title tag to site

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>UMCPC</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
